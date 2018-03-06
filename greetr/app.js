d993c800c8adbfc8b088ab475648c3f7be85cfa3
Nigel Reoch <nigel.reoch@cbc.ca>
2018-03-06 17:32:59 -0500

    Added more

--- greetr/app.js
+++ greetr/app.js
@@ -1,3 +1,9 @@
 
 var g = G$("John", "Doe");
 g.greet().setLang("es").greet(true);
+
+jQuery("#login").click(function() {
+  var loginGrtr = G$("John", "Doe");
+  jQuery("#logindiv").hide();
+  loginGrtr.setLang($("#lang").val()).HTMLGreeting("#greeting", true).log();
+})
